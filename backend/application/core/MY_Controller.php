98792b04a2abc6f9c11d3c3247b4b956e482e8ec
amondocorre <amondocorre@capressocafe.com>
2025-12-15 15:55:22 -0400

    inicio del sistema4

--- backend/application/core/MY_Controller.php
+++ backend/application/core/MY_Controller.php
@@ -109,6 +109,10 @@ class MY_Controller extends CI_Controller
      */
     protected function response($data, $status_code = 200)
     {
+        header('Content-Type: application/json; charset=utf-8');
+        header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+        header('Pragma: no-cache');
+        header('Expires: 0');
         http_response_code($status_code);
         echo json_encode($data, JSON_UNESCAPED_UNICODE);
         exit();
